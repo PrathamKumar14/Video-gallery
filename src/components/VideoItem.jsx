197e1690a308ba3226489272d5695fea1129d6fd
Pratham <prathamsolera@gmail.com>
2020-05-17 22:45:21 +0530

    Add Video list description

--- src/components/VideoItem.jsx
+++ src/components/VideoItem.jsx
@@ -7,6 +7,7 @@ const VideoItem = ({ video }) => {
   return (
     <div className="parent-div">
       <iframe src={videoSrc} title="video player" />
+      <h4 className="description">{video.snippet.title}</h4>
     </div>
   );
 };
