31edba846c21d041562c8aacd2b40cb3574bae46
Pratham <prathamsolera@gmail.com>
2020-05-18 18:38:52 +0530

    Convert class component to functional component

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,39 +1,89 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SearchBar.css";
 
-class SearchBar extends React.Component {
-  state = {
-    searchTerm: ""
-  };
+// class SearchBar extends React.Component {
+//   state = {
+//     searchTerm: ""
+//   };
 
-  handleChange = event => {
-    // console.log(event.target.value);
-    this.setState({ searchTerm: event.target.value });
-  };
+//   handleChange = event => {
+//     // console.log(event.target.value);
+//     this.setState({ searchTerm: event.target.value });
+//   };
 
-  handleSubmit = event => {
-    const { searchTerm } = this.state;
-    const { onFormSubmit } = this.props;
+//   handleSubmit = event => {
+//     const { searchTerm } = this.state;
+//     const { onFormSubmit } = this.props;
 
-    console.log(searchTerm);
-    onFormSubmit(searchTerm);
-    event.preventDefault();
-  };
+//     console.log(searchTerm);
+//     onFormSubmit(searchTerm);
+//     event.preventDefault();
+//   };
 
-  render() {
-    return (
-      <form className="form" onSubmit={this.handleSubmit}>
-        <input
-          onChange={this.handleChange}
-          type="text"
-          placeholder="Search..."
-        />
-        <button className="search-button" type="submit">
-          <i className="fas fa-search" />
-        </button>
-      </form>
-    );
+//   render() {
+//     return (
+//       <form className="form" onSubmit={this.handleSubmit}>
+//         <input
+//           onChange={this.handleChange}
+//           type="text"
+//           placeholder="Search..."
+//         />
+//         <button className="search-button" type="submit">
+//           <i className="fas fa-search" />
+//         </button>
+//       </form>
+//     );
+//   }
+// }
+
+function SearchBar(props) {
+  const { searchTerm, setSearchTerm } = useState("");
+
+  function handleChange(event) {
+    setSearchTerm(event.target.value);
   }
+
+  function handleSubmit(event) {
+    props.onFormSubmit(searchTerm);
+    event.preventDefault();
+  }
+  return (
+    <form className="form" onSubmit={this.handleSubmit}>
+      <input onChange={this.handleChange} type="text" placeholder="Search..." />
+      <button className="search-button" type="submit">
+        <i className="fas fa-search" />
+      </button>
+    </form>
+  );
 }
+
+// function SearchBar(props) {
+//   const { searchTerm, setSearchTerm } = useState("");
+
+//   function handleChange(event) {
+//     const temp = event.target.value;
+//     setSearchTerm(temp);
+//   }
+
+//   const handleSubmit = event => {
+//     // const { searchTerm } = this.state;
+//     // const { onFormSubmit } = this.props;
+
+//     console.log(searchTerm);
+//     props.onFormSubmit(searchTerm);
+//     event.preventDefault();
+//   };
+
+//   return (
+//     <div>
+//       <form className="form" onSubmit={handleSubmit}>
+//         <input onChange={handleChange} type="text" placeholder="Search..." />
+//         <button className="search-button" type="submit">
+//           <i className="fas fa-search" />
+//         </button>
+//       </form>
+//     </div>
+//   );
+// }
 
 export default SearchBar;
